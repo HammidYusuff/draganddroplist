dbda5d77af258e83e267d14452d2ecc68571eff3
HammidYusuff <hammid.yusuff@gmail.com>
2018-01-09 15:13:52 +0100

    Drag and Drop within containers.

--- src/app/list-conponent/list-conponent.component.ts
+++ src/app/list-conponent/list-conponent.component.ts
@@ -6,15 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-conponent.component.css']
 })
 export class ListConponentComponent implements OnInit {
+  
+      containers: Array<Container> = [
+          new Container(1, 'Container 1', [new Widget('1'), new Widget('2')]),
+          new Container(2, 'Container 2', [new Widget('3'), new Widget('4')]),
+          new Container(3, 'Container 3', [new Widget('5'), new Widget('6')])
+      ];
+  
+      widgets: Array<Widget> = [];
+      addTo($event: any) {
+          if ($event) {
+              this.widgets.push($event.dragData);
+          }
+      }
 
-  items = [
-    'item 1',
-    'item 2',
-    'item 3'
-    ];
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+
+class Container {
+  constructor(public id: number, public name: string, public widgets: Array<Widget>) {}
+}
+
+class Widget {
+  constructor(public name: string) {}
 }
